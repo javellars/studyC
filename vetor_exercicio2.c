cda0d0bb6b9503e0ec79cfdf5e76464ac3c2184e
Jullia S. Avelar <50809898+javellars@users.noreply.github.com>
2022-05-11 19:46:52 -0300

    Update vetor_exercicio2.c

--- vetor_exercicio2.c
+++ vetor_exercicio2.c
@@ -2,35 +2,29 @@
 
 
 int main(){
-    float preco, quantidade, lucro;
-    float p,q,l,i;
-    
-while(i>10){
-    scanf("%f", preco[p]);
-    scanf("%f", quantidade[q]);
-}
-
-for(i=0;i>10;i++){
-    lucro=(preco[p]*quantidade[q]);
-    printf("");
-}
-    
-    return 0;
-}
-
-
-/*   
-    float preco[10],quantidade[10],lucro=0.0;
+    float preco[10], lucro=0;
+    int quantidade[10], soma=0;
     int p=0,q=0,i=0;
+ 
+ 
+    for(q=0; q<10;q++){    //leu e armazenou quantidade
+        scanf("%d", &quantidade[q]);
+    }
+       
+    for(p=0; p<10;p++){  //leu e armazenou preço
+        scanf("%f", &preco[p]);
+    }
     
-    scanf("%d", preco[p]);
-    scanf("%d", quantidade[q]);
-
-for(i=0; i<10; i++){
-lucro = preco[p]+quantidade[q];
-    //lucro=((preco[p]*quantidade[q])*10);
-   //printf("O lucro foi de %.10f e a quantidade de produtos vendidos foi de %f\n", lucro, q);
-}
-
-*/
     
+    for(i=0;i<10;i++){ //aqui ele calcula o lucro 
+        lucro+= preco[i]*quantidade[i];
+        
+        soma+=quantidade[i]; //só colocou o vetor dentro de uma caixinha de variável
+    }
+        printf("O lucro foi de  %.3f e a quantidade de produtos vendidos foi de %d\n", lucro, soma );
+     
+     
+     
+        return 0;
+    }
+
