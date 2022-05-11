9fedb1095c37e2308e2ccc009fe9e82866297667
Jullia S. Avelar <50809898+javellars@users.noreply.github.com>
2022-05-11 17:59:06 -0300

    Add files via upload

---
+++ vetor2.c
@@ -0,0 +1,36 @@
+#include <stdio.h> 
+
+
+int main(){
+    float preco, quantidade, lucro;
+    float p,q,l,i;
+    
+while(i>10){
+    scanf("%f", preco[p]);
+    scanf("%f", quantidade[q]);
+}
+
+for(i=0;i>10;i++){
+    lucro=(preco[p]*quantidade[q]);
+    printf("");
+}
+    
+    return 0;
+}
+
+
+/*   
+    float preco[10],quantidade[10],lucro=0.0;
+    int p=0,q=0,i=0;
+    
+    scanf("%d", preco[p]);
+    scanf("%d", quantidade[q]);
+
+for(i=0; i<10; i++){
+lucro = preco[p]+quantidade[q];
+    //lucro=((preco[p]*quantidade[q])*10);
+   //printf("O lucro foi de %.10f e a quantidade de produtos vendidos foi de %f\n", lucro, q);
+}
+
+*/
+    
