847b1e1ece7ec9719453ebdbf900778c139d459d
Jullia S. Avelar <50809898+javellars@users.noreply.github.com>
2022-05-11 16:03:44 -0300

    Update vetor_exercicio1.c

--- vetor_exercicio1.c
+++ vetor_exercicio1.c
@@ -1,20 +1,39 @@
 #include <stdio.h>
 
 int main(){
-    float notas[20], i=0, j=0;
+    float notas[20], j=0;
+    int i=0;
     
     for(i=0; i<20; i++){
-        scanf("%f", &notas);
-        scanf("%f", &j);
-    
-    if(j==i){
-        printf("existe\n");
+        scanf("%f", &notas[i]);
     }
-    else{
-        printf("nao existe\n");
+//até aqui ele recebe e armazena o valor no vetor
+    
+    
+while(j>=0){   //faz rodar enquanto for maior ou igual a 0
+    
+    
+    scanf("%f", &j);//recebe o numero a ser procurado
+    if(j<0){//verifica se o numero a ser procurado é menor que zero, se for ele encerra o programa
+        break;
+    }
+    
+    
+    for(i=0; i<20; i++){ 
+        if((i==19)  && (j!=notas[i])){//aqui ele verifica se a nota existe dentre as que foram digitadas antes
+            printf("nao existe\n");
+        }
+        else{
+            if(j==notas[i]){
+                printf("existe\n");
+                break;//aqui ele para o laço de verificação quando acha se exite ou não
+            }
+        }
+        
     }
 
-    }
+}
+
     return 0;
 }
 
