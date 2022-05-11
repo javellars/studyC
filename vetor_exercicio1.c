ae1531119db7af687d29286f45000a537bdc677f
Jullia S. Avelar <50809898+javellars@users.noreply.github.com>
2022-05-11 12:21:23 -0300

    Add files via upload

---
+++ vetor_exercicio1.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+
+int main(){
+    float notas[20], i=0, j=0;
+    
+    for(i=0; i<20; i++){
+        scanf("%f", &notas);
+        scanf("%f", &j);
+    
+    if(j==i){
+        printf("existe\n");
+    }
+    else{
+        printf("nao existe\n");
+    }
+
+    }
+    return 0;
+}
+
+
+
+
+
+
+ /* for(i=0; i<20; i++){
+        scanf("%f", &j);
+            if(notas==j){
+                printf("existe");
+            }
+            else{
+                printf("não existe");
+            }*/
+
+
+  
+    /*do{
+        scanf("%f", &notas[nota]);//le a nota
+        
+        if(notas[nota]>0){//verifica se é maior que 0
+        printf("existe\n");
+        }
+        
+        else{break;}
+    }
+    while(nota<20);*/
+    
+    
+            /*if(i<20){
+            i++;
+        }
+         if(j<20){
+             j++;
+         }
+        if(j==i){
+            printf("existe\n");
+        }
+        else{
+            printf("nao existe\n");
+        }*/
+    
