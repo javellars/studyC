166e283bf422d41af8c0ddd97b291514421e95db
Jullia S. Avelar <50809898+javellars@users.noreply.github.com>
2022-06-07 10:11:10 -0300

    Update DonRaton_trab.c

--- DonRaton_trab.c
+++ DonRaton_trab.c
@@ -1,34 +1,35 @@
 #include <stdio.h>
 
 int main(){
-    int colelin, c1, c2;
+    int colelin, c1, c2; //declarei as variáveis
 
-    scanf("%d",&colelin);
+    scanf("%d",&colelin);//recebi o valor das variáveis
     
-    int m1[colelin][colelin];
+    int m1[colelin][colelin]; //defini que a matriz vair ter colelin de tamanho para coluna e linha
     
-    for(c1=0;c1<colelin ;c1++){
-        for(c2=0;c2<colelin;c2++){
-            scanf("%d", &m1[c1][c2] );
+    for(c1=0;c1<colelin ;c1++){ //enquanto c1 for menor que a qquantidade de colunas e linhas esse for vai roda
+        for(c2=0;c2<colelin;c2++){//enquanto c2 for menor que a quantidade de colunas e linhas vai rodar esse for
+            scanf("%d", &m1[c1][c2] );// enquanto rodar o for, ele vai receber valores que vão ficar nas posições c1 e c2
         }
     }
     
     for(c1=0; c1<colelin; c1++){
         for(c2=0; c2<colelin; c2++){
-            printf("%d ", m1[c1][c2]);
+            printf("%d ", m1[c1][c2]);//aqui ele vai printar os valores da matriz
         }
-    printf("\n");
+    printf("\n");//printa um espaço
         
     }
     
-    int start,stop;
+    int start,stop;// declara as variáveis para começar a analisar a questão da passagem nas cidades
     
-    scanf("%d", &start);
-    scanf("%d", &stop);
+    scanf("%d", &start);//scanfa a cidade que ele está
+    scanf("%d", &stop);//scanfa a cidade que ele quer ir 
     
-    while(start!=stop){
+    while(start!=stop){//enquanto a cidade de início for diferente da cidade que ele quer chegar 
         for(c1 = 0; c1 < colelin; c1++){
-            if(m1[start][c1] < cidadeAnterior && m1[start][c1] > 0){
+            
+          if(m1[start][c1] < cidadeAnterior && m1[start][c1] > 0){ //se a matriz começando na cidade inicio for menor que a cidadeanterior e matriz menor que zero
                 
             }
                 
