d28a1df4ba2875ea3d82b114dc77da4437670330
Jullia S. Avelar <50809898+javellars@users.noreply.github.com>
2022-07-20 01:00:47 -0300

    Add files via upload

---
+++ ex5.c
@@ -0,0 +1,177 @@
+#include <stdio.h>
+
+int main() {
+  char ch;
+  scanf("%c",&ch);
+
+if(ch=='a'){
+    printf("A");
+}
+else{
+    printf("a");
+}
+
+if(ch=='b'){
+    printf("B");
+}
+else{
+    printf("b");
+}
+
+if(ch=='c'){
+    printf("C");
+}
+else{
+    printf("c");
+}
+
+if(ch=='d'){
+    printf("D");
+}
+else{
+    printf("d");
+}
+
+if(ch=='e'){
+    printf("E");
+}
+else{
+    printf("e");
+}
+
+if(ch=='f'){
+    printf("F");
+}
+else{
+    printf("f");
+}
+
+if(ch=='g'){
+    printf("G");
+}
+else{
+    printf("g");
+}
+
+if(ch=='h'){
+    printf("H");
+}
+else{
+    printf("h");
+}
+
+if(ch=='i'){
+    printf("I");
+}
+else{
+    printf("i");
+}
+
+if(ch=='j'){
+    printf("J");
+}
+else{
+    printf("j");
+}
+
+if(ch=='k'){
+    printf("K");
+}
+else{
+    printf("k");
+}
+
+if(ch=='l'){
+    printf("L");
+}
+else{
+    printf("l");
+}
+
+if(ch=='m'){
+    printf("M");
+}
+else{
+    printf("m");
+}
+
+if(ch=='o'){
+    printf("O");
+}
+else{
+    printf("o");
+}
+
+if(ch=='p'){
+    printf("P");
+}
+else{
+    printf("p");
+}
+
+if(ch=='q'){
+    printf("Q");
+}
+else{
+    printf("q");
+}
+
+if(ch=='r'){
+    printf("R");
+}
+else{
+    printf("r");
+}
+
+if(ch=='s'){
+    printf("S");
+}
+else{
+    printf("s");
+}
+
+if(ch=='t'){
+    printf("T");
+}
+else{
+    printf("t");
+}
+
+if(ch=='v'){
+    printf("V");
+}
+else{
+    printf("v");
+}
+
+if(ch=='w'){
+    printf("W");
+}
+else{
+    printf("w");
+}
+
+if(ch=='x'){
+    printf("X");
+}
+else{
+    printf("x");
+}
+
+if(ch=='y'){
+    printf("Y");
+}
+else{
+    printf("y");
+}
+
+if(ch=='z'){
+    printf("Z");
+}
+else{
+    printf("z");
+}
+
+
+return 0; 
+}
